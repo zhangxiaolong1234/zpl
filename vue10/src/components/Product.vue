1e22b10425a968ce2c5a143fee439bb8b6bd86d3
DESKTOP-5TBM399\lx <1099101814@qq.com>
2019-05-06 17:50:20 +0800

    product创建

---
+++ vue10/src/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="product">
+      <h1>我是商品页</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Product"
+    }
+</script>
+
+<style scoped>
+  .product {
+    width: 100%;
+    height: 70%;
+    text-align: center;
+    background-color: lightskyblue;
+  }
+</style>
--- vue10/src/router/index.js
+++ vue10/src/router/index.js
@@ -3,9 +3,11 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 import Home from '../components/Home';
+import Product from '../components/Product';
 export default new Router({
   routes: [
     {path:'/',redirect:'/home'},
-    {path:'/home',component:Home}
+    {path:'/home',component:Home},
+    {path:'/product',component:Product}
   ]
 })
