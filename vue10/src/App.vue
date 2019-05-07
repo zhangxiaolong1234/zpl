6467319ce93811088b9cdc82b63f53cca2470f3a
DESKTOP-5TBM399\lx <1527836306@qq.com>
2019-05-07 10:57:09 +0800

    233

--- vue10/src/App.vue
+++ vue10/src/App.vue
@@ -14,10 +14,15 @@ export default {
   * {
     margin: 0;
     padding: 0;
+    color: #333;
+    font-weight: 400;
+    font-family: Microsoft Yahei;
+    box-sizing: border-box;
   }
   html,body {
     width: 100%;
     height: 100%;
+    background-color: #f5f5f5;
   }
   #app {
     width: 100%;
